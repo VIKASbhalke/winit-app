be714c88d820922e5416581f7b10c72968d83bfa
Subhadippal006 <subhadippal006@gmail.com>
2020-11-12 18:55:09 +0530

    aligned buttons

--- app/src/main/java/com/example/winit/homepage.java
+++ app/src/main/java/com/example/winit/homepage.java
@@ -8,23 +8,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class homepage extends AppCompatActivity {
-private Button faq_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
 
-        faq_btn=(Button) findViewById(R.id.faq_btn);
-        faq_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openfaqpage();
-            }
-        });
-    }
-    public void openfaqpage(){
-        Intent faq=new Intent(this,faqpage.class);
-        startActivity(faq);
+
 
     }
-}
+
+    }
