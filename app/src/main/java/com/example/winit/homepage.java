60f1b4d73be1549d3284cb2f2b07c09f2a73b387
Subhadippal006 <subhadippal006@gmail.com>
2020-11-12 18:13:49 +0530

    created new pages

--- app/src/main/java/com/example/winit/homepage.java
+++ app/src/main/java/com/example/winit/homepage.java
@@ -2,13 +2,29 @@ package com.example.winit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class homepage extends AppCompatActivity {
-
+private Button faq_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        faq_btn=(Button) findViewById(R.id.faq_btn);
+        faq_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openfaqpage();
+            }
+        });
+    }
+    public void openfaqpage(){
+        Intent faq=new Intent(this,faqpage.class);
+        startActivity(faq);
+
     }
 }
