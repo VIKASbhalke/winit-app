35cd3b46eedcd33d2e5735ce26c320cbe62471b1
Subhadippal006 <subhadippal006@gmail.com>
2020-11-13 09:07:40 +0530

    fixed image buttons

--- app/src/main/java/com/example/winit/homepage.java
+++ app/src/main/java/com/example/winit/homepage.java
@@ -5,17 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+
 
 public class homepage extends AppCompatActivity {
-
+private ImageButton faqs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        faqs=(ImageButton)findViewById(R.id.faqs);
+        faqs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openfaqpage();
+            }
+        });
 
 
-
+    }
+    public  void openfaqpage(){
+        Intent openf=new Intent(this,faqpage.class);
+        startActivity(openf);
     }
 
     }
