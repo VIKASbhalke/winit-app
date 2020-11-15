b35e01d75b4f1599680936a8caaaf69e8cd65968
Subhadippal006 <subhadippal006@gmail.com>
2020-11-15 21:56:58 +0530

    added new features

---
+++ app/src/main/java/com/example/winit/donateActivity.java
@@ -0,0 +1,14 @@
+package com.example.winit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class donateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donate);
+    }
+}
