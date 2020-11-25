6c84a1f6d125f3ef79e80c75af0cec902caee6f7
Subhadippal006 <subhadippal006@gmail.com>
2020-11-25 10:37:47 +0530

    alignment problem fxed

--- app/src/main/java/com/example/winit/calendarActivity.java
+++ app/src/main/java/com/example/winit/calendarActivity.java
@@ -4,12 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class calendarActivity extends AppCompatActivity {
-
+    CalendarView calendar_widget;
+    TextView date_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        calendar_widget = (CalendarView)
+                findViewById(R.id.calendar_widget);
+        date_view = (TextView)
+                findViewById(R.id.date_view);
+
+        // Add Listener in calendar
+        calendar_widget
+                .setOnDateChangeListener(
+                        new CalendarView
+                                .OnDateChangeListener() {
+                            @Override
+
+                            // In this Listener have one method
+                            // and in this method we will
+                            // get the value of DAYS, MONTH, YEARS
+                            public void onSelectedDayChange(
+                                    @NonNull CalendarView view,
+                                    int year,
+                                    int month,
+                                    int dayOfMonth)
+                            {
+
+                                String Date
+                                        = dayOfMonth + "-"
+                                        + (month + 1) + "-" + year;
+
+                                date_view.setText(Date);
+                            }
+                        });
     }
 }
