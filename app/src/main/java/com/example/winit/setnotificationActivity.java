97dfca307a5d4d6b23a830f48455ae98cbea8dfc
Subhadippal006 <subhadippal006@gmail.com>
2020-11-28 18:00:57 +0530

    faq activity bg

---
+++ app/src/main/java/com/example/winit/setnotificationActivity.java
@@ -0,0 +1,14 @@
+package com.example.winit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setnotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setnotification);
+    }
+}
